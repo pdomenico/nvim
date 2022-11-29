8b86da7936f52acb31f131665b7296392fbb9ee3
Domenico Procopio <domenicoprocopio.a@gmail.com>
2022-11-29 19:57:39 +0100

    added copilot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,13 +1,13 @@
 -- Install packer if not already installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -37,58 +37,60 @@ packer.init({
 })
 
 packer.startup(function(use)
-	use "wbthomason/packer.nvim"
+	use("wbthomason/packer.nvim")
 
 	-- Plugins go here
 
 	-- Colorschemes
-	use "folke/tokyonight.nvim"
-	use "lunarvim/darkplus.nvim"
+	use("folke/tokyonight.nvim")
+	use("lunarvim/darkplus.nvim")
 
 	-- Lsp stuff
-	use "neovim/nvim-lspconfig"
-	use "williamboman/mason-lspconfig.nvim"
-	use "williamboman/mason.nvim"
-	use 'simrat39/rust-tools.nvim'
+	use("neovim/nvim-lspconfig")
+	use("williamboman/mason-lspconfig.nvim")
+	use("williamboman/mason.nvim")
+	use("simrat39/rust-tools.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("github/copilot.vim")
 
 	-- cmp
-	use "hrsh7th/nvim-cmp"
-	use "hrsh7th/cmp-buffer"
-	use "hrsh7th/cmp-path"
-	use "hrsh7th/cmp-nvim-lsp"
-	use "hrsh7th/cmp-nvim-lua"
-	use "saadparwaiz1/cmp_luasnip"
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
+	use("saadparwaiz1/cmp_luasnip")
 
 	-- snippets
-	use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
-  use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+	use({ "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" }) --snippet engine
+	use({ "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" }) -- a bunch of snippets to use
 
-	use "windwp/nvim-autopairs" -- autopairs
+	use("windwp/nvim-autopairs") -- autopairs
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-			requires = { {'nvim-lua/plenary.nvim'} }
-	}
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
 	-- treesitter
-	use {
+	use({
 		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate"
-	}
+		run = ":TSUpdate",
+	})
 
 	-- nvim tree
-	use "kyazdani42/nvim-web-devicons"
-	use "kyazdani42/nvim-tree.lua"
+	use("kyazdani42/nvim-web-devicons")
+	use("kyazdani42/nvim-tree.lua")
 
 	-- comments
-	use "numToStr/Comment.nvim"
-	use "JoosepAlviste/nvim-ts-context-commentstring"
+	use("numToStr/Comment.nvim")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
 
 	-- null ls
-	use "jose-elias-alvarez/null-ls.nvim"
 
 	-- toggleterm
-	use "akinsho/toggleterm.nvim"
+	use("akinsho/toggleterm.nvim")
 
 	if packer_bootstrap then
 		require("packer").sync()
